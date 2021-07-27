2d92eae26da6c7b0c77369e037ff450d616ff8e7
Alan Barnett <alanbar@clear.net.nz>
2021-07-27 20:03:07 +1200

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1>Hello {name}! restored copy in Super saved</h1>
+	<h1>Hello {name}! restored copy in Super saved/updated</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
